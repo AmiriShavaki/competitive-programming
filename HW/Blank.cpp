3f2c9d8614f02f006eba4bed186aa8fc74565beb
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2023-04-28 13:13:35 +0330

    Blank template for homeworks

---
+++ HW/Blank.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define N
+//#define int long long
+
+int a[N];
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int q;
+    cin >> q;
+    while(q--) {
+        int n;
+        cin >> n;
+        for (int i = 0; i < n; i++) cin >> a[i];
+    }
+    return 0;
+}
