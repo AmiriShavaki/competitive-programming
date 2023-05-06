d96d3260a9526f889c3ec37793d846e6fc6d339a
Mahdi Amiri <mahdi_amiri98@comp.iust.ac.ir>
2023-05-06 13:38:23 +0330

    Minor change in Blank template

--- contests/Blank.cpp
+++ contests/Blank.cpp
@@ -10,9 +10,9 @@ int32_t main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    int q;
-    cin >> q;
-    while(q--) {
+    int T;
+    cin >> T;
+    while(T--) {
         int n;
         cin >> n;
         for (int i = 0; i < n; i++) cin >> a[i];
